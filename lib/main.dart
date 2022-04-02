d5e856a4e7cb4127865ff0d9397ffc3a74214d69
Doğukan Balaman <dodikbalaman@hotmail.com>
2022-04-02 14:12:04 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text("Heeellloo!"),
