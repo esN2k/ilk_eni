9606626f04761ea7ce24b2e903c14a13b1896b1d
Dogukan Balaman <77047265+esN2k@users.noreply.github.com>
2022-04-03 19:29:04 +0300

    Main (#2)
    * first commit
    
    * main
    
    * commit
    
    * İlk Uygulamam
    
    * Double Widget Test
    
    * Default Text
    
    * Soru Sorma
    
    * Anlamadım
    
    * Wtf
    
    * Json
    
    * Json2
    
    * Sorular
    
    * Random Sorular
    
    * Buton Copy Paste
    
    * Soru Değiştirme
    
    * SOru Değiştirme
    
    * Plugin Update
    
    * Pazar Final
    
    * Deneme2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,80 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unnecessary_string_escapes, prefer_const_literals_to_create_immutables, unused_local_variable, avoid_print, deprecated_member_use
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Scaffold, Buton ve AppBar için gerekli kaynaklar
+import 'dart:math'; //Sorulan sorular için gerekli olan rastgele
 
 // void main() {
 //   runApp(MyApp());
 // }
-void main() => runApp(MyApp());
 
-class MySecondApp extends StatelessWidget {
+//void main() {
+//runApp(MaterialApp(home: MyApp()));
+//}
+
+void main() => runApp(
+      MaterialApp(home: MyApp()),
+    ); //MyApp sonuç verirse main'i çalıştır, vermez ise çalıştırma
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Merhaba"));
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  //State materiel'den gelen bir class
+  int soruIndex = 0;
+
+  void soruCevap() {
+    soruIndex = soruIndex + 1;
+    print(soruIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Random random = Random(); //Random, MyApp'ın hazır tanımladığı bir metod.
+    int soruRandom = random.nextInt(6);
+
+    var sorular = [
+      'What\'s your favorite color?', //0
+      'What\'s your favorite song?', //1
+      'What\'s your favorite animal?', //2
+      'What\'s your favorite artist?', //3
+      'What\'s your favorite movie?', //4
+      'What\'s your favorite activity?', //5
+    ];
+
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => soruCevap(),
+          tooltip: 'Soruyu Değiştir',
+          child: Icon(Icons.refresh),
+        ),
         appBar: AppBar(
           backgroundColor: Colors.lightBlue,
-          title: Text("İlk Uygulamam"),
+          title: Text("İlk Uygulama"),
         ),
-        body: Text("Default Text"),
+        body: Column(
+          children: <Widget>[
+            Text(
+              sorular[soruIndex], //[soruRandom] da kullanılabilir.
+            ), //Sorular artık bir List, Dart'ın otomatik verdiği bir varsayılan nesne, baya methodu var.
+            //Text(sorular.elementAt(randomSoru),), //Liste indexleri 0 ile başlar
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: soruCevap,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 seçildi!'),
+            ),
+            RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Answer 3 seçildi!');
+                }),
+          ],
+        ),
       ),
     );
   }
